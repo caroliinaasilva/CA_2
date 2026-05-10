be96187ebc609f47cd6fab0528ffe82d6871407b
Carolina Silva <carolinadesousa84@yahoo.com>
2026-05-10 16:48:50 +0100

    "Added terminal menu"

--- src/main/java/CA_2/Main.java
+++ src/main/java/CA_2/Main.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package CA_2;
+import java.util.Scanner;
+
 
 /**
  *
@@ -10,4 +12,28 @@ package CA_2;
  */
 public class Main {
     
+    public static void main(String[] args) {
+    // Scanner used to read user input from the terminal
+    Scanner scanner = new Scanner(System.in);
+    MenuOptionEnum selectedOption = null;
+    
+    while (selectedOption == null) {
+    for (MenuOptionEnum option : MenuOptionEnum.values()) {
+        System.out.println(option.getOption() + ". " + option.getLabel());
+}
+   
+    // Ask the user to choose an option from the menu
+    System.out.print("\nSelect an option: ");
+    int userChoice = scanner.nextInt();
+
+    selectedOption = MenuOptionEnum.fromInt(userChoice);
+    
+    if (selectedOption == null) {
+                System.out.println("Invalid option. Please try again.\n");
+            }
+}
+    System.out.println("You selected: " + selectedOption.getLabel());
+
+        scanner.close();
+    }
 }
