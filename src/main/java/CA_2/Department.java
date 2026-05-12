997fb0762ce68c6a158f422def6717e414f25256
Carolina Silva <carolinadesousa84@yahoo.com>
2026-05-12 12:23:34 +0100

    "added department"

---
+++ src/main/java/CA_2/Department.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author carolinasilva
+ */
+public class Department {
+    
+    // Stores the department name
+    private String departmentName;
+
+    // Constructor used to create a department object
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    // Returns the department name
+    public String getDepartmentName() {
+        return departmentName;
+    }
+}
